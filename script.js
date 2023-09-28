64cd0a9b1502c9565b4a62f128163e1eed368655
aaditamanugraha <adhityaaditaman@gmail.com>
2023-09-28 14:38:35 +0800

    Menangkap user input dan merubahnya menjadi object constructor lalu menyimpannya ke dalam array

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-let test = document.getElementById("test");
+// array penyimpanan buku
+const myLibrary = [];
 
-function testCode() {
-  test.innerHTML = "Script sudah connect";
+// object constructor untuk input buku
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // mengambil informasi dari user input
+  let inputTitle = document.getElementById("title").value;
+  let inputAuthor = document.getElementById("author").value;
+  let inputPages = document.getElementById("pages").value;
+
+  // menambahkan nilai input ke dalam array
+  let newBook = new Book(inputAuthor, inputTitle, inputPages);
+
+  // menambahkan newBook ke dalam array myLibrary
+  myLibrary.push(newBook);
+
+  // kosongkan kembali nilai input setelah object constructor ditambahkan
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
 }
