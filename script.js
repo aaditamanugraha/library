ce8371079eca56a4d71f792490d5305adf2d2bd4
aaditamanugraha <adhityaaditaman@gmail.com>
2023-09-28 15:44:36 +0800

    Menambahkan validasi user input harus terisi semua

--- script.js
+++ script.js
@@ -2,30 +2,10 @@
 const myLibrary = [];
 
 // object constructor untuk input buku
-function Book(author, title, pages) {
-  this.author = author;
+function Book(title, author, pages) {
   this.title = title;
+  this.author = author;
   this.pages = pages;
-}
-
-function addBookToLibrary() {
-  // mengambil informasi dari user input
-  let inputTitle = document.getElementById("title").value;
-  let inputAuthor = document.getElementById("author").value;
-  let inputPages = document.getElementById("pages").value;
-
-  // menambahkan nilai input ke dalam array
-  let newBook = new Book(inputAuthor, inputTitle, inputPages);
-
-  // menambahkan newBook ke dalam array myLibrary
-  myLibrary.push(newBook);
-
-  // kosongkan kembali nilai input setelah object constructor ditambahkan
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("pages").value = "";
-
-  showBookList();
 }
 
 function showBookList() {
@@ -51,4 +31,30 @@ function showBookList() {
     containerBookList.appendChild(authorElement);
     containerBookList.appendChild(pagesElement);
   }
+}
+
+function addBookToLibrary() {
+  // mengambil informasi dari user input
+  let inputTitle = document.getElementById("title").value;
+  let inputAuthor = document.getElementById("author").value;
+  let inputPages = document.getElementById("pages").value;
+
+  // validasi user input tidak boleh ada yg kosong
+  if (!inputTitle || !inputAuthor || !inputPages) {
+    alert("Please insert all the input required!");
+    return;
+  }
+
+  // menambahkan nilai input ke dalam array
+  let newBook = new Book(inputAuthor, inputTitle, inputPages);
+
+  // menambahkan newBook ke dalam array myLibrary
+  myLibrary.push(newBook);
+
+  // kosongkan kembali nilai input setelah object constructor ditambahkan
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+
+  showBookList();
 }
